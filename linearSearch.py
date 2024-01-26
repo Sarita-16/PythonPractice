fa1e0110123c8c998db7f56a5a28d41f34b1f4f1
Sarita Mollah <saritamollah@gmail.com>
2024-01-26 12:29:24 +0530

    Linear Search

--- linearSearch.py
+++ linearSearch.py
@@ -1,7 +1,20 @@
+def linearSearch(list, size, key):
+    for i in range (size):
+        if list[i] == key:
+            return (i + 1)
+    return -1
+
 size = int(input("How many number you want to insert in any list : "))
 print("Enter the numbers : ")
 list = []
 for i in range (size):
-    list.append()
+    list.append(int(input()))
 
 key = int(input("Which number you want to searched : "))
+
+result = linearSearch(list, size, key)
+
+if result == -1:
+    print("Number is not found :(")
+else:
+    print(f'The number is found :) at the position of {result}.')
