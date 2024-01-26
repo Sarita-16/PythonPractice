d6a5b0ebdbe16791acef830e9df6bc3306d5db3f
Sarita Mollah <saritamollah@gmail.com>
2024-01-26 11:36:32 +0530

    Initial Commit

---
+++ linearSearch.py
@@ -0,0 +1,7 @@
+size = int(input("How many number you want to insert in any list : "))
+print("Enter the numbers : ")
+list = []
+for i in range (size):
+    list.append()
+
+key = int(input("Which number you want to searched : "))
