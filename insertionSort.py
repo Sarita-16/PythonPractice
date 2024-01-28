d71ff266d1e0204a72a01992d68dc4b3041d5de5
Sarita Mollah <saritamollah@gmail.com>
2024-01-28 11:45:37 +0530

    Insertion Sort : TC : O(n^2)

--- insertionSort.py
+++ insertionSort.py
@@ -1,20 +1,20 @@
 def insertionSort(list, len):
-    for i in range (1, len):
+    for i in range(1, len):
 
         temp = list[i]
         j = i - 1
 
         while j >= 0 and temp < list[j]:
-            list[j+1] = list[j]
-            j-=1
+            list[j + 1] = list[j]
+            j -= 1
 
-        list[j+1] = temp
+        list[j + 1] = temp
 
 
 len = int(input("How many numbers you want to insert : "))
 print("Enter the numbers : ")
 list = []
-for i in range (len):
+for i in range(len):
     list.append(int(input()))
 
 print(f"Before ascending the list {list}")
@@ -22,7 +22,6 @@ print(f"Before ascending the list {list}")
 insertionSort(list, len)
 
 print(f"After ascending the list {list}")
-
 
 '''
     Time Complexity = O(n^2)
