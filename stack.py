dd5d57996f4a3bfdaff0397dde389bc7c6ea062e
Sarita Mollah <saritamollah@gmail.com>
2024-02-05 00:23:44 +0530

    Stack Implementation using Function :)

--- stack.py
+++ stack.py
@@ -5,3 +5,43 @@ from sys import maxsize
 def createStack():
     stack = []
     return stack
+
+
+# Length of stack = 0 the stack is EMPTY
+def isEmpty(stack):
+    return len(stack) == 0
+
+
+# Use Append to ADD item in stack
+def push(stack, item):
+    stack.append(item)
+    print(item + " pushed to stack :) ")
+
+
+# POP removes the topmost or last item in stack
+def pop(stack):
+    if isEmpty(stack):
+        print("Stack Underflow :(")
+        return str(-maxsize - 1)        # return minus infinite
+    return stack.pop()
+
+
+# PEEK
+def peek(stack):
+    if isEmpty(stack):
+        return str(-maxsize - 1)        # return minus infinite
+    return stack[len(stack) - 1]
+
+
+# Driver Program
+stack = createStack()
+
+push(stack, str(5))
+push(stack, str(10))
+push(stack, str(15))
+
+print(pop(stack) + " popped from stack :] ")
+
+print(peek(stack) + " peeked from stack :} ")
+
+
