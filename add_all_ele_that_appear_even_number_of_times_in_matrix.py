bd4f030054685baea4bac1fa3009fcbba048e75f
Sarita Mollah <saritamollah@gmail.com>
2024-02-19 21:29:24 +0530

    Add all element that appear even number of times in matrix

--- add_all_ele_that_appear_even_number_of_times_in_matrix.py
+++ add_all_ele_that_appear_even_number_of_times_in_matrix.py
@@ -1,14 +1,34 @@
+def sumOfEvenOccurrence(matrix):
+    elementCount = {}
+    for row in matrix:
+        for ele in row:
+            if ele in elementCount:
+                elementCount[ele] += 1
+            else:
+                elementCount[ele] = 1
+
+    total_sum = 0
+    for ele, c in elementCount.items():
+        if c % 2 == 0:
+            total_sum += ele * c
+
+    return total_sum
+
+
 size = int(input("Enter the size of Square Matrix : "))
 
 matrix = []
 
+print("Enter the elements : ")
 for i in range(size):
-    col = []
-    for j in range(size):
-        col.append(int(input()))
-    matrix.append(col)
+    row = list(map(int, input().split()))
+    matrix.append(row)
 
-for i in range(size):
-    for j in range(size):
-        print(matrix[i][j], end="  ")
+for row in matrix:
+    for ele in row:
+        print(ele, end="  ")
     print()
+
+ans = sumOfEvenOccurrence(matrix)
+
+print("Sum = ", ans)
