412b341b65b78af35dffa341ce2ebd82a7a5fca6
Sarita Mollah <saritamollah@gmail.com>
2024-02-23 00:43:30 +0530

    Given the row number n. Print the whole Pascal’s triangle. Time Complexity : O(row^2)

---
+++ Pascal's_Triangle_Variation_3.py
@@ -0,0 +1,56 @@
+def nCr(row, col):
+    ans = 1
+    # calculating nCr:
+    for i in range(col):
+        ans = ans * (row - i)
+        ans = ans // (i + 1)
+    return int(ans)
+
+def pascalTriangle(row):
+    ans=[]
+    # Store the entire pascal's triangle:
+    for i in range(1, row+1):
+        tempList = []
+        for j in range(1, i+1):
+            tempList.append(nCr(i-1, j-1))
+        ans.append(tempList)
+    return ans
+
+
+row = int(input("Enter the ROW : "))
+print(f'{row}th row of Pascal Triangle is : ')
+result = pascalTriangle(row)
+for i in result:
+    for ele in i:
+        print(ele, end=" ")
+    print()
+
+
+
+
+
+'''
+
+from typing import *
+
+def pascalTriangle(n : int) -> List[List[int]]:
+
+    def generateRow(row):
+        ans=1
+        ansRow=[]
+
+        ansRow.append(1)
+
+        for col in range (1, row):
+            ans=ans*(row-col)
+            ans=ans//col
+            ansRow.append(ans)
+            return ansRow
+
+    res=[[1]]
+
+    for i in range (2, n+1):
+        res.append(generateRow(i))
+    return res
+
+'''
