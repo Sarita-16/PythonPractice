9132c86c998384a5e8d26bead3dd4325824b0244
Sarita Mollah <saritamollah@gmail.com>
2024-02-29 22:35:36 +0530

    Practice

--- practice.py
+++ practice.py
@@ -37,6 +37,43 @@ print(str(pop(stack)) + " is Popped :] ")
 print(str(peek(stack)) + " is the Top most item :}")        '''
 
 
-for i in range(5):
-    print(i)
 
+'''
+a, b = list(map(int, input(). split(" ")))
+
+if a >= 1 and b >= 1 and a <= 100 and b <= 100:
+    if a <= 10 and b >= 10:
+        print("true", end=" ")
+    else:
+        print("false", end=" ")
+    if a % 2 == 0 or b % 2 == 0:
+        print("true", end=" ")
+    else:
+        print("false", end=" ")
+    if a != b:
+        print("true", end=" ")
+    else:
+        print("false", end=" ")     '''
+
+
+'''
+a = 2
+b = 3
+c = 4
+d = 5
+result = (a + b + c + d)/4
+print(result)
+
+# f to c
+f  = int(input())
+c = (f - 32) * (5/9)
+print(c)        '''
+
+
+a, b = list(map(int, input().split(" ")))
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a, b)
