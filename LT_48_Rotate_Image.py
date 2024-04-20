47b459fc8e5ee33a93b954d54cc26cf08a2d4f3e
Sarita Mollah <saritamollah@gmail.com>
2024-04-21 02:26:23 +0530

    Given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).

---
+++ LT_48_Rotate_Image.py
@@ -0,0 +1,31 @@
+def rotateImage(matrix):
+
+    '''
+    row = len(matrix)
+    col = row
+    transposed = []
+    for j in range(col):
+        temp = []
+        for i in range(row):
+            temp.append(matrix[i][j])
+        transposed.append(temp)
+    rotated = []
+    for r in transposed:
+        rotated.append(r[::-1])
+    return rotated
+    '''
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
+
+    return matrix
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+result = rotateImage(matrix)
+print(result)
