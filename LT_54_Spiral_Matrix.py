2ad59a46f5d27515d00b3e5bbb78a85a5cc3916d
Sarita Mollah <saritamollah@gmail.com>
2024-04-22 21:42:59 +0530

    Spiral Matrix :)

---
+++ LT_54_Spiral_Matrix.py
@@ -0,0 +1,48 @@
+def spiral_order(matrix):
+    if not matrix:
+        return []
+
+    result = []
+    top = 0
+    bottom = len(matrix) - 1
+    left = 0
+    right = len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+        # Traverse top row
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        # Traverse right column
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            # Traverse bottom row
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+
+        if left <= right:
+            # Traverse left column
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+    return result
+
+# Test the function
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+for i in range(len(matrix)):
+    print(matrix[i])
+print(spiral_order(matrix))  # Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+
+
+
+
+
+
+
