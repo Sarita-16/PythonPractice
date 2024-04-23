95eef19c661cea91ccae03b4f733cc5d96fa2b39
Sarita Mollah <saritamollah@gmail.com>
2024-04-23 23:13:50 +0530

    Given two strings s and t, are isomorphic if the characters in s can be replaced to get t.

---
+++ LT_205_Isomorphic_Strings.py
@@ -0,0 +1,21 @@
+def isomorphicStrings(s, t):
+    if len(s) != len(t):
+        return False
+
+    s_to_t = {}
+    t_to_s = {}
+
+    for char_s, char_t in zip(s, t):
+        if char_s not in s_to_t and char_t not in t_to_s:
+            s_to_t[char_s] = char_t
+            t_to_s[char_t] = char_s
+        elif s_to_t.get(char_s) != char_t or t_to_s.get(char_t) != char_s:
+            return False
+
+    return True
+
+
+s = "bbbaaaba"
+t = "aaabbbba"
+result = isomorphicStrings(s, t)
+print(result)
