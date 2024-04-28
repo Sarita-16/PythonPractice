51cfbeb2f2d1291be949aa3c588a15d25b768a0b
Sarita Mollah <saritamollah@gmail.com>
2024-04-28 22:31:28 +0530

    Tree : Path Sum

---
+++ LT_112_Path_Sum.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def hasPathSum(root, targetSum):
+    if not root:
+        return False
+
+    if not root.left and not root.right:
+        return targetSum == root.val
+
+    return hasPathSum(root.left, targetSum - root.val) or hasPathSum(root.right, targetSum - root.val)
+
+
+
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.right = TreeNode(8)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.right = TreeNode(1)
+
+targetSum = 22
+print(hasPathSum(root, targetSum))
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+
