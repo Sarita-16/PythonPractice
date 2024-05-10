935581b5b0d770843937f872687c7d3d2f597a8f
Sarita Mollah <saritamollah@gmail.com>
2024-05-10 19:55:00 +0530

    Linked List Cycle

---
+++ LT_141_Linked_List_Cycle.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if not fast or not fast.next:
+            return False
+        slow = slow.next
+        fast = fast.next.next
+
+    return True
+
+# Example usage:
+# Create the linked list
+head = ListNode(3)
+head.next = ListNode(2)
+head.next.next = ListNode(0)
+head.next.next.next = ListNode(-4)
+head.next.next.next.next = head.next  # Creating a cycle
+
+print(hasCycle(head))  # Output: True
