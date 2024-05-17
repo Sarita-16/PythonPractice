4d6c5230120b557f60d25ea25a34530059ec79d4
Sarita Mollah <saritamollah@gmail.com>
2024-05-17 21:39:39 +0530

    Zigzag Conversion :)

---
+++ LT_6_Zigzag_Conversion.py
@@ -0,0 +1,19 @@
+def convert(s, numRows):
+    if numRows == 1 or numRows >= len(s):
+        return s
+
+    rows = [''] * numRows
+    curRow = 0
+    goingDown = False
+
+    for char in s:
+        rows[curRow] += char
+        if curRow == 0 or curRow == numRows - 1:
+            goingDown = not goingDown
+        curRow += 1 if goingDown else -1
+
+    return ''.join(rows)
+
+s = "PAYPALISHIRING"
+numRows = 3
+print("Zigzag Conversion : ", convert(s, numRows))
