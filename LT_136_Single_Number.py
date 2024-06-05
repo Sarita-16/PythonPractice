2df54150ca22c0d2cf80afef32714bfd387307c0
Sarita Mollah <saritamollah@gmail.com>
2024-06-05 22:10:18 +0530

    Single number in a list.  (❁´◡`❁)

---
+++ LT_136_Single_Number.py
@@ -0,0 +1,12 @@
+import functools
+
+def singleNumber(nums):
+    number = 0
+    for i in nums:
+        number ^= i
+
+    return number
+
+
+nums = list(map(int, input("Enter the numbers : ").split()))
+print("Single element in the list is : ", singleNumber(nums))
